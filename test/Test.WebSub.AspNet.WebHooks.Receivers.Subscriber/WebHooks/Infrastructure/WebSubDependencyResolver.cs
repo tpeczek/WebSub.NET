94c5c3eeedf62eb20f1212776f8b9eff2b6b7c9a
Tomasz Pęczek <tpeczek@gmail.com>
2018-08-09 22:01:38 +0200

    Intent verification handling tests (#15)

---
+++ test/Test.WebSub.AspNet.WebHooks.Receivers.Subscriber/WebHooks/Infrastructure/WebSubDependencyResolver.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+using WebSub.WebHooks.Receivers.Subscriber.Services;
+
+namespace Test.WebSub.AspNet.WebHooks.Receivers.Subscriber.WebHooks.Infrastructure
+{
+    internal class WebSubDependencyResolver : IDependencyResolver
+    {
+        private class DictionaryWebSubSubscriptionsStore : IWebSubSubscriptionsStore
+        {
+            private readonly IDictionary<string, WebSubSubscription> _store;
+
+            public DictionaryWebSubSubscriptionsStore(IDictionary<string, WebSubSubscription> store)
+            {
+                _store = store;
+            }
+
+            public Task<WebSubSubscription> CreateAsync()
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task<WebSubSubscription> CreateAsync(CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task RemoveAsync(string id)
+            {
+                return RemoveAsync(id, CancellationToken.None);
+            }
+
+            public Task RemoveAsync(string id, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task RemoveAsync(WebSubSubscription subscription)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task RemoveAsync(WebSubSubscription subscription, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task<WebSubSubscription> RetrieveAsync(string id)
+            {
+                return RetrieveAsync(id, CancellationToken.None);
+            }
+
+            public Task<WebSubSubscription> RetrieveAsync(string id, CancellationToken cancellationToken)
+            {
+                WebSubSubscription subscription = _store.ContainsKey(id) ? _store[id] : null;
+
+                return Task.FromResult(subscription);
+            }
+
+            public Task UpdateAsync(WebSubSubscription subscription)
+            {
+                return UpdateAsync(subscription, CancellationToken.None);
+            }
+
+            public Task UpdateAsync(WebSubSubscription subscription, CancellationToken cancellationToken)
+            {
+                return Task.FromResult<Object>(null);
+            }
+        }
+
+        private static readonly Type _webSubSubscriptionsStoreType = typeof(IWebSubSubscriptionsStore);
+
+        private readonly IWebSubSubscriptionsStore _webSubSubscriptionsStore;
+
+        public WebSubDependencyResolver()
+            : this(new Dictionary<string, WebSubSubscription>())
+        { }
+
+        public WebSubDependencyResolver(IDictionary<string, WebSubSubscription> webSubSubscriptionsStore)
+        {
+            _webSubSubscriptionsStore = new DictionaryWebSubSubscriptionsStore(webSubSubscriptionsStore);
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == _webSubSubscriptionsStoreType)
+            {
+                return _webSubSubscriptionsStore;
+            }
+
+            return null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- test/Test.WebSub.AspNet.WebHooks.Receivers.Subscriber/WebHooks/WebSubWebHookReceiverTests.cs
+++ test/Test.WebSub.AspNet.WebHooks.Receivers.Subscriber/WebHooks/WebSubWebHookReceiverTests.cs
@@ -1,6 +1,71 @@
-﻿namespace Test.WebSub.AspNet.WebHooks.Receivers.Subscriber.WebHooks
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Collections.Generic;
+using Xunit;
+using WebSub.WebHooks.Receivers.Subscriber.Services;
+using WebSub.AspNet.WebHooks.Receivers.Subscriber.WebHooks;
+using Test.WebSub.AspNet.WebHooks.Receivers.Subscriber.WebHooks.Infrastructure;
+
+namespace Test.WebSub.AspNet.WebHooks.Receivers.Subscriber.WebHooks
 {
     public class WebSubWebHookReceiverTests
     {
+        #region Fields
+        private const string WEBHOOK_BASE_URI = "/api/webhooks/incoming/websub/";
+
+        private const string WEBHOOK_ID = "73481a8e-c9ee-4ec4-89e3-b25b3179ae92";
+        private const string OTHER_WEBHOOK_ID = "24bacd35-cf3e-4ffe-811c-6278d339c11d";
+        #endregion
+
+        #region Prepare SUT
+        private WebSubDependencyResolver PrepareWebSubDependencyResolver(
+            string subscriptionId,
+            WebSubSubscriptionState subscriptionState = WebSubSubscriptionState.SubscribeValidated,
+            string secret = null)
+        {
+            return new WebSubDependencyResolver(new Dictionary<string, WebSubSubscription>
+            {
+                { subscriptionId, new WebSubSubscription { Id = subscriptionId, State = subscriptionState, Secret = secret } }
+            });
+        }
+
+        private HttpRequestContext PrepareWebSubRequestContext(WebSubDependencyResolver dependencyResolver)
+        {
+            return new HttpRequestContext
+            {
+                Configuration = new HttpConfiguration
+                {
+                    DependencyResolver = dependencyResolver
+                }
+            };
+        }
+
+        private HttpRequestMessage PrepareWebSubRequestMessage(string method, string id, HttpRequestContext context)
+        {
+            HttpRequestMessage webSubRequestMessage = new HttpRequestMessage(new HttpMethod(method), WEBHOOK_BASE_URI + id);
+
+            webSubRequestMessage.SetRequestContext(context);
+
+            return webSubRequestMessage;
+        }
+        #endregion
+
+        #region Tests
+        [Theory]
+        [InlineData("GET")]
+        [InlineData("POST")]
+        public async void OnReceiveAsync_WebSubRequestWithoutMatchingId_ReturnsNotFoundResponse(string httpMethod)
+        {
+            HttpRequestContext context = PrepareWebSubRequestContext(PrepareWebSubDependencyResolver(WEBHOOK_ID));
+            HttpRequestMessage request = PrepareWebSubRequestMessage(httpMethod, WEBHOOK_ID, context);
+            WebSubWebHookReceiver webSubWebHookReceiver = new WebSubWebHookReceiver();
+
+            HttpResponseMessage receiveAsyncResult = await webSubWebHookReceiver.ReceiveAsync(OTHER_WEBHOOK_ID, context, request);
+
+            Assert.Equal(HttpStatusCode.NotFound, receiveAsyncResult.StatusCode);
+        }
+        #endregion
     }
 }
